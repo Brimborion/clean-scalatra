9f7b3831559a86a9d072dbd0d319a98d761af2b3
Charles Guérin-Rouard <charles.guerin.rouard@protonmail.com>
2020-03-29 16:50:19 +0200

    wip

--- src/main/scala/com/brimborion/modules/catalog/adapters/controllers/AuthorController.scala
+++ src/main/scala/com/brimborion/modules/catalog/adapters/controllers/AuthorController.scala
@@ -1,9 +1,14 @@
 package com.brimborion.modules.catalog.adapters.controllers
 
+import java.util.UUID
+
 import com.brimborion.core.controllers.CustomController
+import com.brimborion.core.exceptions.NotFoundException
 import com.brimborion.modules.catalog.adapters.controllers.dtos.{AuthorDto, PostAuthorDto}
 import com.brimborion.modules.catalog.domain.usecases.AuthorUseCases
-import org.scalatra.Ok
+import org.scalatra.{NotFound, Ok}
+
+import scala.concurrent.Future
 
 class AuthorController(private val authorUseCases: AuthorUseCases) extends CustomController {
   post("/") {
@@ -11,5 +16,15 @@ class AuthorController(private val authorUseCases: AuthorUseCases) extends Custo
 
     authorUseCases.addAuthor(name = postAuthorDto.name, birthDate = postAuthorDto.birthDate)
       .map(author => Ok(AuthorDto(author)))
+  }
+
+  get("/:id") {
+    val authorId = UUID.fromString(params("id"))
+
+    authorUseCases.getAuthor(authorId)
+      .map(author => Ok(AuthorDto(author)))
+      .recoverWith{
+        case e: NotFoundException => Future.successful(NotFound(e.message))
+      }
   }
 }
--- src/main/scala/com/brimborion/modules/catalog/domain/usecases/AuthorUseCases.scala
+++ src/main/scala/com/brimborion/modules/catalog/domain/usecases/AuthorUseCases.scala
@@ -1,6 +1,7 @@
 package com.brimborion.modules.catalog.domain.usecases
 
 import java.time.LocalDate
+import java.util.UUID
 
 import com.brimborion.modules.catalog.domain.entities.Author
 import com.brimborion.modules.catalog.domain.usecases.interfaces.AuthorRepository
@@ -9,4 +10,6 @@ import scala.concurrent.Future
 
 class AuthorUseCases(private val authorRepository: AuthorRepository) {
   def addAuthor(name: String, birthDate: LocalDate): Future[Author] = authorRepository.create(name, birthDate)
+
+  def getAuthor(authorId: UUID): Future[Author] = authorRepository.find(authorId)
 }
