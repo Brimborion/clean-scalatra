805c8bf744eae0e7daddb53b8f38fd03976cb907
Charles Guérin-Rouard <charles.guerin.rouard@protonmail.com>
2020-04-06 15:40:32 +0200

    Add missing routes in API

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,4 +1,6 @@
 import com.atlassian.oai.validator.OpenApiInteractionValidator
+import com.atlassian.oai.validator.whitelist.ValidationErrorsWhitelist
+import com.atlassian.oai.validator.whitelist.rule.WhitelistRules
 import com.brimborion.core.module.Module
 import com.brimborion.modules.catalog.config.CatalogModule
 import com.brimborion.modules.user.config.UserModule
@@ -16,7 +18,16 @@ class ScalatraBootstrap extends LifeCycle {
     module.getControllers.foreach(controller => context.mount(controller.implementation, controller.urlPattern))
 
   private def openApiValidator: OpenApiInteractionValidator = {
-    OpenApiInteractionValidator.createFor("openapi.yaml").build
+    val whitelist = ValidationErrorsWhitelist.create()
+      .withRule(
+        "Avoid unknown errors",
+        WhitelistRules.messageHasKey("validation.request.body.schema.unknownError"
+      ))
+
+    OpenApiInteractionValidator
+      .createFor("openapi.yaml")
+      .withWhitelist(whitelist)
+      .build
   }
 }
 
--- src/main/scala/com/brimborion/modules/catalog/adapters/controllers/BookItemController.scala
+++ src/main/scala/com/brimborion/modules/catalog/adapters/controllers/BookItemController.scala
@@ -3,13 +3,13 @@ package com.brimborion.modules.catalog.adapters.controllers
 import com.brimborion.core.controllers.CustomController
 import com.brimborion.modules.catalog.adapters.controllers.dtos.{BookItemDto, PostBookItemDto}
 import com.brimborion.modules.catalog.domain.usecases.BookItemUseCases
-import org.scalatra.Ok
+import org.scalatra.{Created, Ok}
 
 class BookItemController(private val bookItemUseCases: BookItemUseCases) extends CustomController {
   post("/") {
     val postBookItemDto = parsedBody.extract[PostBookItemDto]
 
     bookItemUseCases.addBookItem(bookIsbn = postBookItemDto.bookIsbn, tag = postBookItemDto.tag)
-      .map(bookItem => Ok(BookItemDto(bookItem)))
+      .map(bookItem => Created(BookItemDto(bookItem)))
   }
 }
